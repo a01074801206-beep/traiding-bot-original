7020579aaf597954b31f935d7cd45bb2416906bb
a01074801206-beep <a01074801206@gmail.com>
2026-03-20 10:55:53 +0900

    Delete original_model directory

--- original_model/src/models/dqn_trading_model.py
+++
@@ -1,9 +0,0 @@
-import os
-import torch
-from src.utils.paths import CHECKPOINT_DIR
-
-class DQNAgent:
-    def save(self, filename):
-        # 경로를 CHECKPOINT_DIR로 강제 지정
-        save_path = os.path.join(CHECKPOINT_DIR, filename)
-        torch.save(self.model.state_dict(), save_path)
